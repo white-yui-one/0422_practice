b92aaba18055bd9f88a3cbbf7150ffcd63c1c158
ロックマン <110light03@gmail.com>
2015-04-22 04:52:17 +0000

    add sample03

---
+++ sample03.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    print('現在は'.date('G時i分s秒').'です');
+    ?>
+</body>
+</html>
